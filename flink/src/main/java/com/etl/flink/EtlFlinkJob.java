577a980f10ee67b2c04ea80c69fd4f5b06143a49
aboutas <athanasiosmpoutas@gmail.com>
2025-11-03 12:59:00 +0200

    Change window size = 10

--- flink/src/main/java/com/etl/flink/EtlFlinkJob.java
+++ flink/src/main/java/com/etl/flink/EtlFlinkJob.java
@@ -148,7 +148,7 @@ public class EtlFlinkJob {
                         result.getJobId() != null ? result.getJobId() : "unknown",
                         result.getAggregationType() != null ? result.getAggregationType() : "none"
                 ))
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(3)))
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
                 .aggregate(new WindowAggregator());
 
         // Union results
